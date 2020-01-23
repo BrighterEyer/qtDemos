f3b9ff3a1b8f28a7a5a1c4af604b526f3867f4f6
brightereyer2 <brightereyer2@163.com>
2020-01-23 22:21:58 +0800

    + 百度地图

---
+++ MSVC/bMapDemo/mainwindow.cpp
@@ -0,0 +1,85 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "bridge.h"
+#include <QWebChannel>
+#include <QStackedLayout>
+#include <QDebug>
+
+#define bridgeins bridge::instance()
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    InitForm();
+
+    connect(&m_timer, SIGNAL(timeout()), this, SLOT(onTimeOut()));
+
+    m_timer.start(1000);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::InitForm()
+{
+    QWebChannel *channel = new QWebChannel(this);
+    channel->registerObject("bridge", (QObject*)bridgeins);
+
+
+    webView = new QWebEngineView(this);
+    webView->page()->setWebChannel(channel);
+    webView->page()->load(QUrl(qApp->applicationDirPath() + "/index.html"));
+
+
+    QString msg =  qApp->applicationDirPath()+"/index.html";
+    qDebug() << msg;
+
+    QStackedLayout* layout = new QStackedLayout(ui->frame);
+//    ui->frame->setLayout(layout);
+    layout->addWidget(webView);
+
+    bridgeins->Init();
+
+    const QList<Light_t>& list = bridgeins->GetLightList();
+    int nCount = list.count();
+    for(int i=0; i<nCount; i++)
+    {
+        Light_t tLight = list[i];
+        ui->cbLight->addItem(tLight.strDesc, tLight.strName);
+    }
+    ui->cbLight->setCurrentIndex(0);
+
+
+    this->showMaximized();
+}
+
+
+void MainWindow::on_btnAddLight_clicked()
+{
+    Light_t tLight = bridgeins->AddLight();
+    ui->cbLight->addItem(tLight.strDesc, tLight.strName);
+    ui->cbLight->setCurrentIndex(ui->cbLight->count()-1);
+    bridgeins->onUpdateData();
+}
+
+void MainWindow::on_btnLightOn_clicked()
+{
+    QString strName = ui->cbLight->currentData().toString();
+    bridgeins->onLightOn(strName);
+}
+
+void MainWindow::on_btnLightOff_clicked()
+{
+    QString strName = ui->cbLight->currentData().toString();
+    bridgeins->onLightOff(strName);
+}
+
+void MainWindow::onTimeOut()
+{
+    //  bridgeins->onUpdateStatus();
+}
---
+++ MSVC/bMapDemo/mainwindow.h
@@ -0,0 +1,37 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QWebEngineView>
+#include <QTimer>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void onTimeOut();
+
+    void on_btnAddLight_clicked();
+
+    void on_btnLightOn_clicked();
+
+    void on_btnLightOff_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QTimer m_timer;
+    void InitForm();
+
+    QWebEngineView *webView;
+};
+
+#endif // MAINWINDOW_H
