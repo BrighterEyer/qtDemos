490b76b6ded5e410e5ce25ea0368e9543ae9baa4
brightereyer2 <brightereyer2@163.com>
2020-01-23 22:15:46 +0800

    + 轮播图

---
+++ CarouselDemo/CarouselImageWindow/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <QtWidgets/QApplication>
+#include "CarouselImageWindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    CarouselImageWindow w;
+    w.addImage(":/Resources/1.jpg");
+
+    w.addImage(":/Resources/images1.jpg");
+
+    w.addImage(":/Resources/2.jpg");
+
+    w.addImage(":/Resources/images2.jpg");
+    w.addImage(":/Resources/3.jpg");
+
+    w.addImage(":/Resources/images3.jpg");
+    w.addImage(":/Resources/4.jpg");
+
+
+
+    w.addImage(":/Resources/images4.jpg");
+
+
+    w.addImage(":/Resources/5.jpg");
+    w.addImage(":/Resources/images5.jpg");
+    w.addImage(":/Resources/6.jpg");
+    w.addImage(":/Resources/7.jpg");
+    w.addImage(":/Resources/8.jpg");
+
+
+
+
+    w.setControlButtonColor(Qt::yellow);
+    w.setBorderColor(Qt::red);
+    w.setImageChangeDuration(2000);
+    w.startPlay();
+    w.show();
+    return a.exec();
+}
