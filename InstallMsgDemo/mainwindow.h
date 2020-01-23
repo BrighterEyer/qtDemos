3c9dac1ac06fd96c68ac81d2447bbe1634f9319a
brightereyer2 <brightereyer2@163.com>
2020-01-23 20:09:22 +0800

    + 全局日志输出

---
+++ InstallMsgDemo/mainwindow.h
@@ -0,0 +1,32 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtMessageHandler>
+#include <QDateTime>
+#include <QMutex>
+#include <QtDebug>
+#include <QFile>
+#include <QTextStream>
+#include <QProgressBar>
+#include <QProgressDialog>
+#include <QMessageBox>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    void myMessageOutput(QtMsgType, const QMessageLogContext&, const QString&);
+};
+
+#endif // MAINWINDOW_H
