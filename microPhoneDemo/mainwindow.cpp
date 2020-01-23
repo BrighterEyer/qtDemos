4e32a459f8ae2228463d8827af4279a94e889d18
brightereyer2 <brightereyer2@163.com>
2020-01-23 21:04:24 +0800

    + 麦克风录音

---
+++ microPhoneDemo/mainwindow.cpp
@@ -0,0 +1,50 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    audioRecorder = new QAudioRecorder;
+    QStringList inputs = audioRecorder->audioInputs();
+    ui->comboBox->addItems(inputs);
+    /* 显示录制时间*/
+    connect(audioRecorder,&QAudioRecorder::durationChanged,this,&MainWindow::updateProgress);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+// 开始按钮
+void MainWindow::on_pushButton_clicked()
+{
+    audioRecorder->setAudioInput(ui->comboBox->currentText());
+    audioRecorder->setOutputLocation(QUrl::fromLocalFile(ui->lineEdit->text()));
+    audioRecorder->record();
+}
+// 停止按钮
+void MainWindow::on_pushButton_2_clicked()
+{
+    audioRecorder->stop();
+}
+// 选择按钮
+void MainWindow::on_pushButton_3_clicked()
+{
+    QString filename = QFileDialog::getSaveFileName();
+    ui->lineEdit->setText(filename);
+}
+
+
+//显示时间槽函数实现
+void MainWindow::updateProgress(qint64 duration)
+{
+    if(audioRecorder->error() != QMediaRecorder::NoError || duration < 1000)
+        return;
+    setWindowTitle(tr("Recorded %1 sec").arg(duration / 1000));
+}
+
---
+++ microPhoneDemo/mainwindow.h
@@ -0,0 +1,35 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMainWindow>
+#include <QAudioRecorder>
+#include <QDebug>
+#include <QFileDialog>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QAudioRecorder *audioRecorder;
+
+private slots:
+    /* 显示记录时间 */
+    void updateProgress(qint64 duration);
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+
+};
+
+#endif // MAINWINDOW_H
