7ec976ba94c24d41edc4cd81853c950a2927a8d6
brightereyer2 <brightereyer2@163.com>
2020-01-23 18:29:29 +0800

    * http下载管理器

---
+++ SingleDownloadManager/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <QApplication>
+#include "downloadmanager.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    DownloadManager w;
+    w.start();
+    w.show();
+
+    return a.exec();
+}
