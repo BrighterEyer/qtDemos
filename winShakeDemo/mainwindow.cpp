691f2baf334514123e11b8e13753b94615f661ab
brightereyer2 <brightereyer2@163.com>
2020-01-23 22:18:24 +0800

    + 窗口下坠、抖动、透明 效果

---
+++ winShakeDemo/mainwindow.cpp
@@ -0,0 +1,67 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPropertyAnimation>
+#include <QApplication>
+#include <QGuiApplication>
+#include <QDesktopWidget>
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//抖动效果
+void MainWindow::onDropWindow()
+{
+    QPropertyAnimation *pAnimation = new QPropertyAnimation(this, "geometry");
+
+    QDesktopWidget *pDesktopWidget = QApplication::desktop();
+    QRect rect = pDesktopWidget->availableGeometry();
+    int x = (rect.width() - width()) / 2;
+    int y = (rect.height() - height()) / 2;
+
+    pAnimation->setDuration(1000);
+    pAnimation->setStartValue(QRect(x, 0, width(), height()));
+    pAnimation->setEndValue(QRect(x, y, width(), height()));
+    pAnimation->setEasingCurve(QEasingCurve::OutElastic);
+    pAnimation->start(QAbstractAnimation::DeleteWhenStopped);
+}
+
+//下坠效果
+void MainWindow::onShakeWindow()
+{
+    QPropertyAnimation *pAnimation = new QPropertyAnimation(this, "pos");
+    pAnimation->setDuration(500);
+    pAnimation->setLoopCount(2);
+    pAnimation->setKeyValueAt(0, QPoint(geometry().x() - 3, geometry().y() - 3));
+    pAnimation->setKeyValueAt(0.1, QPoint(geometry().x() + 6, geometry().y() + 6));
+    pAnimation->setKeyValueAt(0.2, QPoint(geometry().x() - 6, geometry().y() + 6));
+    pAnimation->setKeyValueAt(0.3, QPoint(geometry().x() + 6, geometry().y() - 6));
+    pAnimation->setKeyValueAt(0.4, QPoint(geometry().x() - 6, geometry().y() - 6));
+    pAnimation->setKeyValueAt(0.5, QPoint(geometry().x() + 6, geometry().y() + 6));
+    pAnimation->setKeyValueAt(0.6, QPoint(geometry().x() - 6, geometry().y() + 6));
+    pAnimation->setKeyValueAt(0.7, QPoint(geometry().x() + 6, geometry().y() - 6));
+    pAnimation->setKeyValueAt(0.8, QPoint(geometry().x() - 6, geometry().y() - 6));
+    pAnimation->setKeyValueAt(0.9, QPoint(geometry().x() + 6, geometry().y() + 6));
+    pAnimation->setKeyValueAt(1, QPoint(geometry().x() - 3, geometry().y() - 3));
+    pAnimation->start(QAbstractAnimation::DeleteWhenStopped);
+}
+
+//透明度效果
+void MainWindow::onOpacityWindow()
+{
+    QPropertyAnimation *pAnimation = new QPropertyAnimation(this, "windowOpacity");
+    pAnimation->setDuration(1000);
+    pAnimation->setKeyValueAt(0, 1);
+    pAnimation->setKeyValueAt(0.5, 0);
+    pAnimation->setKeyValueAt(1, 1);
+    pAnimation->start(QAbstractAnimation::DeleteWhenStopped);
+}
