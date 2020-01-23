b2ab2be53c87b7f82d31f523b33077ae547ead05
brightereyer2 <brightereyer2@163.com>
2020-01-23 20:07:17 +0800

    + 进度条

---
+++ progressDemo/SliderDemo.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#ifndef SLIDERDEMO_H
+#define SLIDERDEMO_H
+
+#include <QMainWindow>
+#include <QTimer>
+
+namespace Ui {
+class SliderDemo;
+}
+
+class SliderDemo : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit SliderDemo(QWidget *parent = nullptr);
+    ~SliderDemo();
+
+public slots:
+    void slotStart();
+    void slotStop();
+    void slotProcessBar();
+
+private:
+    Ui::SliderDemo *ui;
+    QTimer *m_timer;
+};
+
+#endif // SLIDERDEMO_H
