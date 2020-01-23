0d0d3e61c04494482f0e184a2069d8b161b81d2f
brightereyer2 <brightereyer2@163.com>
2020-01-23 20:00:14 +0800

    + 按钮、标签 右键菜单加点击事件

---
+++ compRightClickMenu/mainwindow.cpp
@@ -0,0 +1,68 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QDebug>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //初始化动作
+    buttonAction1 = new QAction("btnFirstAction",this);
+    buttonAction2 = new QAction("btnSecondAction",this);
+
+    lableAction1 = new QAction("lbFirstAction",this);
+    lableAction2 = new QAction("lbSecondAction",this);
+
+    //初始化菜单
+    buttonMenu = new QMenu(this);
+
+    lableMenu = new QMenu(this);
+    //动作添加到菜单
+    buttonMenu->addAction(buttonAction1);
+    buttonMenu->addAction(buttonAction2);
+
+    lableMenu->addAction(lableAction1);
+    lableMenu->addAction(lableAction2);
+
+    //给动作设置信号槽
+    connect( buttonAction1, &QAction::triggered, [=]()
+    {
+        qDebug()<<"I'm btnFirstAction";
+    });
+    connect( buttonAction2, &QAction::triggered, [=]()
+    {
+        qDebug()<<"I'm btnSecondAction";
+    });
+    connect( lableAction1, &QAction::triggered, [=]()
+    {
+        qDebug()<<"I'm lbFirstAction";
+    });
+    connect( lableAction2, &QAction::triggered, [=]()
+    {
+        qDebug()<<"I'm lbSecondAction";
+    });
+
+    //给控件设置上下文菜单策略
+    ui->label->setContextMenuPolicy(Qt::CustomContextMenu);
+    //鼠标右键点击控件时会发送一个void QWidget::customContextMenuRequested(const QPoint &pos)信号
+    //给信号设置相应的槽函数
+    connect(ui->label,&QLabel::customContextMenuRequested,[=](const QPoint &pos)
+    {
+        qDebug()<<pos;//参数pos用来传递右键点击时的鼠标的坐标，这个坐标一般是相对于控件左上角而言的
+        lableMenu->exec(QCursor::pos());
+    });
+
+    ui->pushButton->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(ui->pushButton,&QPushButton::customContextMenuRequested,[=](const QPoint &pos)
+    {
+        qDebug()<<pos;
+        buttonMenu->exec(QCursor::pos());
+    });
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ compRightClickMenu/mainwindow.h
@@ -0,0 +1,32 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<QAction>
+#include<QMenu>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    //声明动作
+    QAction * buttonAction1;
+    QAction * buttonAction2;
+
+    QAction * lableAction1;
+    QAction * lableAction2;
+    //声明菜单
+    QMenu * buttonMenu;
+    QMenu * lableMenu;
+};
+
+#endif // MAINWINDOW_H
