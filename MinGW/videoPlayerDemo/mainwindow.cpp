d59f4d83637e5fbfce050e119bf917cfb3c38086
brightereyer2 <brightereyer2@163.com>
2020-02-06 22:32:55 +0800

    + 视频播放器demo

---
+++ MinGW/videoPlayerDemo/mainwindow.cpp
@@ -0,0 +1,39 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    player = new QMediaPlayer;
+    videoWidget = new QVideoWidget;
+    player->setVideoOutput(videoWidget);
+
+    ui->verticalLayout->addWidget(videoWidget);
+    connect(ui->startButton,SIGNAL(clicked()),this,SLOT(startVideo()));
+    connect(ui->pauseButton,SIGNAL(clicked()),this,SLOT(pauseVideo()));
+    connect(ui->closeButton,SIGNAL(clicked()),this,SLOT(closeVideo()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::startVideo()
+{
+    player->setMedia(QUrl::fromLocalFile("G:\\workspaces\\qt\\videoSources\\player.flv"));
+    videoWidget->show();
+    player->play();
+}
+
+void MainWindow::pauseVideo()
+{
+    player->pause();
+}
+
+void MainWindow::closeVideo()
+{
+    player->stop();
+}
---
+++ MinGW/videoPlayerDemo/mainwindow.h
@@ -0,0 +1,35 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QUrl>
+//要包含下面的两个文件，必须在.pro文件中添加  QT += multimedia  multimediawidgets
+#include <QMediaPlayer>
+#include <QVideoWidget>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QMediaPlayer *player;
+    QVideoWidget *videoWidget;
+    QMediaPlaylist *medialist;
+
+public slots:
+    void startVideo();
+    void pauseVideo();
+    void closeVideo();
+};
+
+#endif // MAINWINDOW_H
+
